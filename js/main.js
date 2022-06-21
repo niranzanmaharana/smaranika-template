459c393ffe1616d2eaf5decb062437b1b407acb5
Niranjan Maharana <75609938+niranzanmaharana@users.noreply.github.com>
2022-06-21 10:05:38 +0530

    images, css and js files

---
+++ js/main.js
@@ -0,0 +1,23 @@
+(function($) {
+	'use strict';
+
+	var nav_offset_top = $('header').height() + 50;
+	/*-------------------------------------------------------------------------------
+	  Navbar 
+	-------------------------------------------------------------------------------*/
+
+	//* Navbar Fixed
+	function navbarFixed() {
+		if ($('.header-section').length) {
+			$(window).scroll(function() {
+				var scroll = $(window).scrollTop();
+				if (scroll >= nav_offset_top) {
+					$('.header-section').addClass('navbar-fixed');
+				} else {
+					$('.header-section').removeClass('navbar-fixed');
+				}
+			});
+		}
+	}
+	navbarFixed();
+})(jQuery);
