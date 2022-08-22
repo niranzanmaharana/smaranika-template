3496de3a0ab43267501d7b382351170a74da3a46
Niranjan Maharana <niranjan.maharana@signify.com>
2022-08-22 16:13:02 +0530

    Added pages
    Added about, contact, event and blogs pages

--- js/main.js
+++ js/main.js
@@ -9,14 +9,14 @@
 	//* Navbar Fixed
 	function navbarFixed() {
 		if ($('.header-section').length) {
-			$(window).scroll(function() {
+			/*$(window).scroll(function() {
 				var scroll = $(window).scrollTop();
 				if (scroll >= nav_offset_top) {
 					$('.header-section').addClass('navbar-fixed');
 				} else {
 					$('.header-section').removeClass('navbar-fixed');
 				}
-			});
+			});*/
 		}
 	}
 	navbarFixed();
